09106db4a217d7d9ab431f2c13733a0e2a30492c
strupani <strupani@ON34C02217498>
2017-12-01 16:01:09 -0500

    convert to .NET Core 2.0 console app and add slide deck

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
 namespace SudokuSolver
